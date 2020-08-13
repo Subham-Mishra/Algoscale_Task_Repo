4b5ba5f9a3d16561c9bed757b8fafebd3ef8edc0
Subham Mishra <imsubham882@gmail.com>
2020-08-14 00:17:42 +0530

    Completed Assigned Tasks
    1.  An Admin login page
    2.  Validate ID and Password
    3.  If Valid, redirect to the profile page.
    4.  If Invalid, redirect to a Signup page.
    5.  Post Login/Signup, display all existing User ID on profile page using a dropdown list.
    6.  Delete button to delete the selected User ID from the database.
    7.  Logout.

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,11 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup_page/',views.signuppage,name='signup_page'),
+    path('login_page/',views.loginpage,name='login_page'),
+    path('login/',views.handleLogin,name='login'),
+    path('logout/',views.handleLogout,name='logout'),
+    path('signup/',views.handleSignup,name='signup'),
+    path('delete/<str:pk>', views.delete, name='delete')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    params={}
+    users = User.objects.all()
+    params={'users':users}
     return render(request,'users/index.html',params)
+
+def loginpage(request):
+    return render(request,'users/login.html')
+
+def signuppage(request):
+    return render(request,'users/signup.html')
+
+
+
+def handleLogin(request):
+    if request.method == 'POST':
+        # Getting POST data
+        username = request.POST['user_name']
+        password = request.POST['user_password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('/signup_page')
+    else:
+        return redirect('index')
+
+
+def handleLogout(request):
+    logout(request)
+    return redirect('index')
+
+def handleSignup(request):
+    if request.method == 'POST':
+        # Getting POST data
+        username = request.POST['user_name']
+        email = request.POST['user_email']
+        pass_1 = request.POST['user_password_1']
+        pass_2 = request.POST['user_password_2']
+
+        # if pass_1 != pass_2:
+        #     messages.error(request, "Passwords don't match")
+        #     return redirect('/')
+
+        myuser = User.objects.create_user(username, email, pass_1)
+        myuser.save()
+        user = authenticate(username=username, password=pass_1)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('/signup_page')
+    else:
+        return redirect('index')
+
+
+def delete(request,pk):
+    userDelete = User.objects.get(id=pk)
+    userDelete.delete()
+    return redirect('/logout')
+    
